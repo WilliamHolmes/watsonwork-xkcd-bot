eab0ed5d46a03ec893c0ac01dfab4345970fb581
William Holmes <holmesw@ie.ibm.com>
2018-07-11 15:48:16 +0100

    Change dest location

--- js/constants.js
+++ js/constants.js
@@ -2,7 +2,7 @@ const constants = {
     COLOR_ERROR: '#C12228',
     regex: {
         KEY: 'https://xkcd.com/',
-        IMG: 'https://xkcd.com/comics/',
+        IMG: 'https://imgs.xkcd.com/comics/',
         XKCD: /https:\/\/xkcd.com\/([0-9]*)/gi
     },
     TEMP_DIR: './temp_files'
