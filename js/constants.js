c0bc1c4468ef5e4ac31bdfbd410329c632996f00
William Holmes <holmesw@ie.ibm.com>
2018-07-11 19:22:32 +0100

    update reg ex

--- js/constants.js
+++ js/constants.js
@@ -2,7 +2,7 @@ const constants = {
     COLOR_ERROR: '#6E7B91',
     regex: {
         IMG: 'https://imgs.xkcd.com/comics/',
-        XKCD: /https:\/\/xkcd.com\/([0-9]*)/gmi
+        XKCD: /https:\/\/xkcd.com\/([0-9a-z]*)/gmi
     },
     TEMP_DIR: './temp_files'
 }
